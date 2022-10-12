3a5767ed2498ece96e2e5e39e8f3679e3412166a
unknown <jessespeks@hotmail.com>
2022-10-11 21:37:42 -0400

    generated models and connected files through migration using sequelize cli

--- server.js
+++ server.js
@@ -8,20 +8,20 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-// SEQUELIZE CONNECTION
-        const sequelize = new Sequelize({
-          storage: process.env.PG_URI,
-          dialect: 'postgres',
-          username: 'postgres',
-          password: 'Password'
-        })
+// // SEQUELIZE CONNECTION
+//         const sequelize = new Sequelize({
+//           storage: process.env.PG_URI,
+//           dialect: 'postgres',
+//           username: 'postgres',
+//           password: 'Password'
+//         })
 
-try {
-    sequelize.authenticate() 
-    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
-} catch(err) {
-    console.log(`Unable to connect to PG: ${err}`) 
-}
+// try {
+//     sequelize.authenticate() 
+//     console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+// } catch(err) {
+//     console.log(`Unable to connect to PG: ${err}`) 
+// }
 
         
 // ROOT
